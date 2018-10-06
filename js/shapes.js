f1bab3f01babaf71dc2abf4e04de121d40381d10
nalingaddis <nalin.gaddis@gmail.com>
2018-10-06 15:09:48 -0400

    drawRect()

--- js/shapes.js
+++ js/shapes.js
@@ -1,12 +1,17 @@
-//nxnxn cube
-function drawRect(n, position, materialID){
-  for(i = 0; i < n; i++){
-    for(j = 0; j < n; j++){
-      for(k = 0; k < n; k++){
+//nxmxo cube
+function drawRect(dimension, position, materialID){
+  for(i = 0; i < dimension.x; i++){
+    for(j = 0; j < dimension.y; j++){
+      for(k = 0; k < dimension.z; k++){
           updateArray(map3d, gridCoords(position.x + i, position.y + j, position.z + k), materialID)
       }
     }
   }
 
   drawIso()
+}
+
+//nxnxn cube
+function drawNCube(n, position, materialID){
+  drawRect(gridCoords(n,n,n), position, materialID)
 }
