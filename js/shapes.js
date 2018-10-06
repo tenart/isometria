d5cefc8e8c6d35512327298589c5b75548422703
nalingaddis <nalin.gaddis@gmail.com>
2018-10-06 15:41:53 -0400

    gave up on pyramid

--- js/shapes.js
+++ js/shapes.js
@@ -3,15 +3,15 @@ function drawRect(dimension, position, materialID){
   for(i = 0; i < dimension.x; i++){
     for(j = 0; j < dimension.y; j++){
       for(k = 0; k < dimension.z; k++){
-          updateArray(map3d, gridCoords(position.x + i, position.y + j, position.z + k), materialID)
+          updateArray(map3d, gridCoords(position.x + i, position.y + j, position.z + k), materialID);
       }
     }
   }
 
-  drawIso()
+  drawIso();
 }
 
 //nxnxn cube
 function drawNCube(n, position, materialID){
-  drawRect(gridCoords(n,n,n), position, materialID)
+  drawRect(gridCoords(n,n,n), position, materialID);
 }
