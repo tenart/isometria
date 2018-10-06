9b99d147b352989583acd35721da1f526241ffcb
Tom <tomn98@live.unc.edu>
2018-10-06 19:17:22 -0400

    Updated GUI

--- js/levels.js
+++ js/levels.js
@@ -1,7 +1,11 @@
 function level1() {
-    drawNCube(3, gridCoords(5,5,0), 1);
-    updateArray(map3d, gridCoords(7,7,2), 101);
-    updateArray(map3d, gridCoords(7,7,0), 103);
+    drawNCube(4, gridCoords(5,5,0), 1);
+    updateArray(map3d, gridCoords(8,8,3), 0);
+    updateArray(map3d, gridCoords(7,8,3), 101);
+    updateArray(map3d, gridCoords(8,7,3), 102);
+    updateArray(map3d, gridCoords(8,8,0), 0);
+    updateArray(map3d, gridCoords(7,8,0), 103);
+    updateArray(map3d, gridCoords(8,7,0), 103);
     drawIso();
 }
 
